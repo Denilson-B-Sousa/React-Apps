460b4ba7b5dee0cd69233decb11bc43dd34c7cee
Denilson B. Sousa <99680537+Denilson-B-Sousa@users.noreply.github.com>
2023-03-02 16:50:19 -0300

    add Navbar

--- youtube_clone/src/App.jsx
+++ youtube_clone/src/App.jsx
@@ -1,11 +1,31 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Box } from '@mui/material'
 import React from 'react';
+/*Atalho para criar a estrurura básica é
+rafce
+*/
 
-function App() {
-    return (
-        <div>
-            <p>App</p>
-        </div>
-    )
-}
+
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components/Index.js';
+
+
+const App = () => (
+    <BrowserRouter>
+        <Box sx={{backgroundColor:'#000'}}>
+            Navbar
+            <Routes>
+                <Route path="/" exact element={<Feed/>}/>
+                <Route path="/video/:id" element={<VideoDetail/>}/>
+                <Route path="/channel/:id" element={<ChannelDetail/>}/>
+                <Route path="/search/:serchTerm" element={<SearchFeed/>}/>
+                <Route path="/video/:id" element={<VideoDetail/>}/>
+
+
+            </Routes>
+        </Box>
+    </BrowserRouter>
+);
+   
 
 export default App;
+
