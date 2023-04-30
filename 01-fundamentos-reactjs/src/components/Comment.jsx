7169c833ddc167559c9f57408da1a26c8469c3c7
Denilson B. Sousa <99680537+Denilson-B-Sousa@users.noreply.github.com>
2023-04-30 18:40:28 -0300

    adicionando like
    adicionando like aos comentários.

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,8 +1,21 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from '../../src/Coment.module.css'
 import { Avatar } from './Avatar'
+import { useState } from 'react';
 
-export function Comment(){
+export function Comment({content, deleteComment}){
+
+    const [likeCount, setLikeCount] = useState(0); //Estado para o like
+
+    function handleDeleteComment(){
+        deleteComment(content)
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        })
+    }
     return(
         <div className={styles.comment}>
             <Avatar src="https://github.com/Denilson-B-Sousa.png"/>
@@ -13,17 +26,17 @@ export function Comment(){
                              <strong>Denilson</strong>
                              <time className={styles.authorAndTime} title="06 de Março às 10:22" dateTime='2023-03-06 10:22:30'>Cerca de 1 hora atrás</time>
                         </div>
-                        <button title="Deletar Comentário">
+                        <button onClick={handleDeleteComment} title="Deletar Comentário" >
                                  <Trash size={24}/>
                         </button>
                     </header>
 
-                <p>Muito bem devo, parabéns!! </p>
+                <p>{content}</p>
     </div>
             <footer>
-                <button title="Aplaudir Post">
+                <button onClick={handleLikeComment} title="Aplaudir Post">
                     <ThumbsUp />
-                    Aplaudir <span>23</span>
+                    Aplaudir <span>{likeCount}</span>
                 </button>
             </footer>
         </div>
--- 01-fundamentos-reactjs/src/components/Posts.jsx
+++ 01-fundamentos-reactjs/src/components/Posts.jsx
@@ -3,15 +3,30 @@ import styles from '../../src/Post.module.css'
 
 import {format, formatDistanceToNow} from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { useState } from 'react'
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
+import DOMPurify from 'dompurify'
 
 //author: [avatar_url = "", name= "", role: ""]
 //publish: Date
 //content
 
+const comments = [
+  1,
+  2,
+
+];
+
+
 export function Post({author, publishedAt, content}){
 
+  const [comments, setComments] = useState([
+    'Post, interessante!'
+  ])
+
+  const [newComment, setNewComment] = useState('')
+    
     const publishedDateFormat= format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
       locale: ptBR,
     })
@@ -21,6 +36,36 @@ export function Post({author, publishedAt, content}){
       addSuffix: true,
       
     })
+
+    function handleCreateNewComment(){
+        event.preventDefault()
+        const  newCommentText = event.target.comment.value
+
+        setComments([...comments, newCommentText]);
+
+        setNewCommentText('');
+
+    }
+
+    function handleNewCommentChange(){
+      event.target.setCustomValidity('')
+      setNewCommentText(event.target.value);
+    }
+
+    function deleteComment(commentToDelete){
+      const commentWithoutDeleteOne = comments.filter(comment => {
+        return comment !== commentToDelete;
+      })
+      
+      setComments(commentWithoutDeleteOne);
+    }
+
+    function handleNewCommentInvalid(){
+        event.target.setCustomValidity('Esse campo é obrigatório!')
+    }
+
+    const isNewCommentEmpty = newComment.length === 0;
+
     return(
     <article className={styles.post}>
       <header>
@@ -38,27 +83,37 @@ export function Post({author, publishedAt, content}){
       <div className={styles.content}>
           {content.map(line => {
             if (line.type === 'paragraph'){
-              return <p>{line.content}</p>;
+              return <p key={line.content}>{line.content}</p>;
             } else if (line.type === 'link') {
-              return <p><a href="#">{line.content}</a></p>
+              return <p key={line.content}><a href="#">{line.content}</a></p>
             }
           })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
           <strong>Deixe seu Feedback</strong>
 
-          <textarea placeholder='Deixe seu comentário'>
-
-          </textarea>
+          <textarea 
+          placeholder='Deixe seu comentário'
+          name='comment'
+          value={newComment}
+          onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required={true}
+          />
           <footer>
-              <button type='submit'>Publicar</button>
+              <button type='submit' disabled={isNewCommentEmpty}>Publicar</button>
           </footer>
       </form>
       <div className={styles.commentList}>
-          <Comment/>
-          <Comment/>
-          <Comment/>
+          {comments.map(comment => {
+            return <Comment 
+              content={comment} 
+              key={comment} 
+              deleteComment={deleteComment}
+              
+            />
+          })}
       </div>
     </article>
   )
